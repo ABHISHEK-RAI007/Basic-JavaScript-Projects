ef6db92daf6f952a8af9b564faf503fcece3d528
Abhishek Rai <abhishek.rai0522@gmail.com>
2022-06-01 18:42:13 +0530

    Add files via upload

---
+++ KelvinWeather.js
@@ -0,0 +1,37 @@
+/*
+// Forecast of temprature
+const kelvin  = 293;
+
+// Converting kelvin into Celsius
+const celsius = kelvin -273;
+
+// Converting Celsius into Fahrenheit
+let fahrenheit = celsius * (9 / 5) +32;
+
+// round down 
+fahrenheit = Math.floor(fahrenheit);
+
+console.log (`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+*/
+
+// Forecast of temprature
+const kelvin  = 0;
+
+// Converting Kelvin into Celsius
+const celsius = kelvin -273;
+
+// Converting Celsius into Fahrenheit
+let fahrenheit = celsius * (9 / 5) +32;
+
+// round down 
+fahrenheit = Math.floor(fahrenheit);
+
+console.log (`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+
+// Converting into Newton
+let newton = celsius * (33/100);
+
+// Round down
+newton = Math.floor(newton);
+
+console.log (`The temperature is ${newton} degrees Newton.`);
